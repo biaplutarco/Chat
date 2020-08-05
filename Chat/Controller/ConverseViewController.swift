953ae2dd14372a0dd3711623d53fea67ab52b6bf
Bia <biaplutarco@gmail.com>
2020-08-05 18:23:07 -0300

    create controllers files

---
+++ Chat/Controller/ConverseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ConverseViewController.swift
+//  Chat
+//
+//  Created by Bia Plutarco on 05/08/20.
+//  Copyright © 2020 Bia Plutarco. All rights reserved.
+//
+
+import UIKit
+
+class ConverseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
