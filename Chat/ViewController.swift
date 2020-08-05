969c6e75193dddad4d5c11b46dd1ac9a327be77a
Bia <biaplutarco@gmail.com>
2020-08-05 16:58:39 -0300

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Bia Plutarco on 05/08/20.
+//  Copyright © 2020 Bia Plutarco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
